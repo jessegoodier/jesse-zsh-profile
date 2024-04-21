1789305d08113308074413b09c3115f182da3b29
jesse goodier <31039225+jessegoodier@users.noreply.github.com>
2024-04-21 14:51:02 -0400

    spring cleaning

--- .kube-scripts/k-remove-bad-contexts.sh
+++ .kube-scripts/k-remove-bad-contexts.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Get all contexts
-
-cp ~/.kube/config ./kubeconfig
+cp ~/.kube/config ./kubeconfig-backup-"$(date +"%d-%m-%Y-%H-%M-%S")"
 KUBECONFIG=./kubeconfig
 
 contexts=$(kubectl config get-contexts -o name)
@@ -24,25 +23,3 @@ for context in $contexts; do
     echo ""
 done
 unset KUBECONFIG
-
-k-remove-bad-contexts() { # remove dead contexts
-cp ~/.kube/config ./kubeconfig-"`date +"%d-%m-%Y-%H-%M-%S"`"
-contexts=$(kubectl config get-contexts -o name)
-
-# Loop through each context
-for context in $contexts; do
-    echo "Context: $context"
-    kubectl config use-context $context
-
-    # Check if the context is working
-    if kubectl get pods &> /dev/null; then
-        echo "Context $context is working"
-    else
-        echo "Context $context is not working"
-        kubectl config delete-context $context
-        echo "--------------Context $context has been removed------------------"
-    fi
-
-    echo ""
-done
-}
